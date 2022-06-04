ace61f3fa98f67d01dd73110f36b6209471645c0
Vel-Cognit-OV <69151320+Vel-Cognit-OV@users.noreply.github.com>
2022-06-04 22:06:20 +0530

    updated

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,6 +1,7 @@
 import React,{Component} from "react";
 
 class QuizQuestion extends React.Component{
+
     render(){
         return(
             <main>
@@ -9,7 +10,7 @@ class QuizQuestion extends React.Component{
                 </section>
                 <section className="buttons">
                 <ul>
-                    <li>{this.props.quiz_question.answer_options[0]}</li>
+                    <li>{this.props.quiz_question}</li>
                 </ul>
                 </section>
             </main>
